084195e6ba80664f4e6159f4a13daecb397f2c97
shaurmyau <152178185+shaurmyau@users.noreply.github.com>
2025-03-04 09:16:43 +0300

    Add files via upload

---
+++ din_lib/main_dynamic.c
@@ -0,0 +1,10 @@
+   #include <stdio.h>
+   #include "power_divide.h"
+
+   int main() {
+       int base = 2;
+       int exponent = 10;
+       int result = power_divide(base, exponent);
+       printf("2^10 using dynamic library: %d\n", result);
+       return 0;
+   }
---
+++ din_lib/power_divide.h
@@ -0,0 +1,6 @@
+   #ifndef POWER_DIVIDE_H
+   #define POWER_DIVIDE_H
+
+   int power_divide(int base, int exponent);
+
+   #endif // POWER_DIVIDE_H
